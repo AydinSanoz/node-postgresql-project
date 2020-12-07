c06cf5b011c2909740dcd1ec19088167edaf7f26
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-08 00:19:41 +0300

    .env file created for saple usage

--- modal/UserModal.js
+++ modal/UserModal.js
@@ -3,7 +3,17 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 // postgresql connection check
 
+const { DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_PORT, DB_NAME } = process.env;
+console.log("🚀 ~ file: UserModal.js ~ line 7 ~ DB_USERNAME", DB_USERNAME)
+console.log("🚀 ~ file: UserModal.js ~ line 9 ~ DB_PASSWORD", DB_PASSWORD)
+console.log("🚀 ~ file: UserModal.js ~ line 11 ~ DB_HOST", DB_HOSTNAME)
+console.log("🚀 ~ file: UserModal.js ~ line 13 ~ DB_PORT", DB_PORT)
+console.log("🚀 ~ file: UserModal.js ~ line 15 ~ DB_NAME", DB_NAME)
+// sample usage to connect db ---> 'postgres://postgres:090519@localhost:5001/canan'
+
+
 const sequelize = new Sequelize(
+    // `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`
 	'postgres://postgres:090519@localhost:5001/canan'
 );
 
